29260cc9bdbf18230eff46460e2e405131279bee
defects4j <defects4j@localhost>
2023-03-09 18:19:02 +0900

    add Jsoup52

--- src/main/java/org/jsoup/parser/XmlTreeBuilder.java
+++ src/main/java/org/jsoup/parser/XmlTreeBuilder.java
@@ -3,9 +3,13 @@ package org.jsoup.parser;
 import org.jsoup.helper.Validate;
 import org.jsoup.nodes.*;
 
-import java.util.Iterator;
+import java.util.List;
 
 /**
+ * Use the {@code XmlTreeBuilder} when you want to parse XML without any of the HTML DOM rules being applied to the
+ * document.
+ * <p>Usage example: {@code Document xmlDoc = Jsoup.parse(html, baseUrl, Parser.xmlParser());}</p>
+ *
  * @author Jonathan Hedley
  */
 public class XmlTreeBuilder extends TreeBuilder {
@@ -13,6 +17,7 @@ public class XmlTreeBuilder extends TreeBuilder {
     protected void initialiseParse(String input, String baseUri, ParseErrorList errors) {
         super.initialiseParse(input, baseUri, errors);
         stack.add(doc); // place the document onto the stack. differs from HtmlTreeBuilder (not on stack)
+        doc.outputSettings().syntax(Document.OutputSettings.Syntax.xml);
     }
 
     @Override
@@ -63,7 +68,16 @@ public class XmlTreeBuilder extends TreeBuilder {
 
     void insert(Token.Comment commentToken) {
         Comment comment = new Comment(commentToken.getData(), baseUri);
-        insertNode(comment);
+        Node insert = comment;
+        if (commentToken.bogus) { // xml declarations are emitted as bogus comments (which is right for html, but not xml)
+            // so we do a bit of a hack and parse the data as an element to pull the attributes out
+            String data = comment.getData();
+            if (data.length() > 1 && (data.startsWith("!") || data.startsWith("?"))) {
+                String declaration = data.substring(1);
+                insert = new XmlDeclaration(declaration, comment.baseUri(), data.startsWith("!"));
+            }
+        }
+        insertNode(insert);
     }
 
     void insert(Token.Character characterToken) {
@@ -86,9 +100,8 @@ public class XmlTreeBuilder extends TreeBuilder {
         String elName = endTag.name();
         Element firstFound = null;
 
-        Iterator<Element> it = stack.descendingIterator();
-        while (it.hasNext()) {
-            Element next = it.next();
+        for (int pos = stack.size() -1; pos >= 0; pos--) {
+            Element next = stack.get(pos);
             if (next.nodeName().equals(elName)) {
                 firstFound = next;
                 break;
@@ -97,15 +110,17 @@ public class XmlTreeBuilder extends TreeBuilder {
         if (firstFound == null)
             return; // not found, skip
 
-        it = stack.descendingIterator();
-        while (it.hasNext()) {
-            Element next = it.next();
-            if (next == firstFound) {
-                it.remove();
+        for (int pos = stack.size() -1; pos >= 0; pos--) {
+            Element next = stack.get(pos);
+            stack.remove(pos);
+            if (next == firstFound)
                 break;
-            } else {
-                it.remove();
-            }
         }
+    }
+
+    List<Node> parseFragment(String inputFragment, String baseUri, ParseErrorList errors) {
+        initialiseParse(inputFragment, baseUri, errors);
+        runParser();
+        return doc.childNodes();
     }
 }
