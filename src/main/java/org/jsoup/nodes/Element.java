4b1e5670ccda65901d55fe1b59f9c312c392db04
defects4j <defects4j@localhost>
2023-03-09 18:19:09 +0900

    add Jsoup70

--- src/main/java/org/jsoup/nodes/Element.java
+++ src/main/java/org/jsoup/nodes/Element.java
@@ -1085,11 +1085,13 @@ public class Element extends Node {
     }
 
     static boolean preserveWhitespace(Node node) {
-        // looks only at this element and one level up, to prevent recursion & needless stack searches
+        // looks only at this element and five levels up, to prevent recursion & needless stack searches
         if (node != null && node instanceof Element) {
-            Element element = (Element) node;
-            return element.tag.preserveWhitespace() ||
-                element.parent() != null && element.parent().tag.preserveWhitespace();
+            Element el = (Element) node;
+                if (el.tag.preserveWhitespace())
+                    return true;
+                else
+                    return el.parent() != null && el.parent().tag.preserveWhitespace();
         }
         return false;
     }
