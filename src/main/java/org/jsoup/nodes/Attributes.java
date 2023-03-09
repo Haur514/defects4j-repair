b8a2fb59d195b3ca8d63a99e3a87ea3ba2ff3d77
defects4j <defects4j@localhost>
2023-03-09 18:19:04 +0900

    add Jsoup57

--- src/main/java/org/jsoup/nodes/Attributes.java
+++ src/main/java/org/jsoup/nodes/Attributes.java
@@ -119,7 +119,8 @@ public class Attributes implements Iterable<Attribute>, Cloneable {
         Validate.notEmpty(key);
         if (attributes == null)
             return;
-        for (String attrKey : attributes.keySet()) {
+        for (Iterator<String> it = attributes.keySet().iterator(); it.hasNext(); ) {
+            String attrKey = it.next();
             if (attrKey.equalsIgnoreCase(key))
                 attributes.remove(attrKey);
         }
